b53b7623b103677f9ddac8981012c9bc07dc74e7
DockyD <35370698+DockyD@users.noreply.github.com>
2019-01-21 11:58:58 +0100

    gen changes

--- loadStimuli.m
+++ loadStimuli.m
@@ -5,21 +5,12 @@ global G;
 % A1, A2, A3
 G.stim1 = cell(3, 1);       % First stimulus: A and C
 
-G.stim1{1} = double(imread([G.objPath, G.subjCfg.stimFiles.A{1}])) * (G.maxWhite/255);
-G.stim1{2} = double(imread([G.objPath, G.subjCfg.stimFiles.A{2}])) * (G.maxWhite/255);
-G.stim1{3} = double(imread([G.objPath, G.subjCfg.stimFiles.A{3}])) * (G.maxWhite/255);
+G.stim1{1} = double(imread([G.objPath, G.subjCfg.stimFiles.A])) * (G.maxWhite/255);
+G.stim1{2} = double(imread([G.objPath, G.subjCfg.stimFiles.B])) * (G.maxWhite/255);
 
-% B1, B2
-G.stim2 = cell(2, 1);       % Second stimulus: B and D
+%contrast images generated here?
 
-G.stim2{1} = double(imread([G.objPath, G.subjCfg.stimFiles.B{1}])) * (G.maxWhite/255);
-G.stim2{2} = double(imread([G.objPath, G.subjCfg.stimFiles.B{2}])) * (G.maxWhite/255);
+%mask generated here
 
-% Duckies
-G.stimDucky = cell(G.nDuckies, 1);
-
-for iDucky = 1:G.nDuckies
-    G.stimDucky{iDucky} = double(imread([G.duckyPath, 'Ducky', num2str(iDucky), '.jpg'])) * (G.maxWhite/255);
-end
 
 end
---
+++ rsTrial.m
@@ -0,0 +1,118 @@
+function [data, log] = rsTrial(cfg0)
+
+global G;
+
+%% Initialize variables
+data = [];
+data.resp = nan;
+data.RT = nan;
+data.correct = nan;
+
+nextOnset = cfg0.nextOnset;
+
+log = [];
+log.cfg0 = cfg0;
+log.onsets = [];
+
+%% Present stimulus before oddball
+pStim = Screen('MakeTexture', G.pWindow, G.stim2{cfg0.stim});
+Screen('DrawTexture', G.pWindow, pStim);
+
+% Superimpose fixation
+Screen('DrawDots', G.pWindow, [0, 0], G.fixDotSize*G.pixPerDeg, 0, G.screenCenter, 1);
+Screen('DrawDots', G.pWindow, [0, 0], 0.5*G.fixDotSize*G.pixPerDeg, G.fixDotColor, G.screenCenter, 1);
+
+% Flip
+time = Screen('Flip', G.pWindow, nextOnset - G.flipLag);
+G.B.sendTrigger(G.triggers.locBlock.stimOn + cfg0.stim); %adds to trigger value 
+log.onsets = [log.onsets, time];
+
+nextOnset = time + G.locBlock.stimDurPreOddball;
+
+%% Present stimulus and oddball
+Screen('DrawTexture', G.pWindow, pStim);
+
+% Superimpose fixation
+Screen('DrawDots', G.pWindow, [0, 0], G.fixDotSize*G.pixPerDeg, 0, G.screenCenter, 1);
+if ~cfg0.oddball
+    Screen('DrawDots', G.pWindow, [0, 0], 0.5*G.fixDotSize*G.pixPerDeg, G.fixDotColor, G.screenCenter, 1);
+end
+
+% Flip
+time = Screen('Flip', G.pWindow, nextOnset - G.flipLag);
+G.B.sendTrigger(G.triggers.locBlock.oddballOn + cfg0.oddball);
+log.onsets = [log.onsets, time];
+timeOnset = time;
+
+nextOnset = time + G.locBlock.oddballDur;
+
+cfg = [];
+cfg.validKeys = G.validKeys;
+cfg.endTime = nextOnset - G.pressLag;    
+[data.resp, log.respTime] = getResponse(cfg);
+
+%% Present stimulus
+Screen('DrawTexture', G.pWindow, pStim);
+
+% Superimpose fixation
+Screen('DrawDots', G.pWindow, [0, 0], G.fixDotSize*G.pixPerDeg, 0, G.screenCenter, 1);
+Screen('DrawDots', G.pWindow, [0, 0], 0.5*G.fixDotSize*G.pixPerDeg, G.fixDotColor, G.screenCenter, 1);
+
+% Flip
+time = Screen('Flip', G.pWindow, nextOnset - G.flipLag);
+log.onsets = [log.onsets, time];
+
+nextOnset = time + G.locBlock.stimDurPostOddball;
+
+if isnan(data.resp)
+    cfg = [];
+    cfg.validKeys = G.validKeys;
+    cfg.endTime = nextOnset - G.pressLag;    
+    [data.resp, log.respTime] = getResponse(cfg);
+end
+
+%% Present post-stimulus fixation dot
+Screen('DrawDots', G.pWindow, [0, 0], G.fixDotSize*G.pixPerDeg, 0, G.screenCenter, 1);
+Screen('DrawDots', G.pWindow, [0, 0], 0.5*G.fixDotSize*G.pixPerDeg, G.fixDotColor, G.screenCenter, 1);
+
+G.B.sendTrigger(G.triggers.locBlock.stimOff);
+time = Screen('Flip', G.pWindow, nextOnset - G.flipLag);
+log.onsets = [log.onsets, time];
+
+%{
+%% Present mask
+if cfg0.mask == 1  
+    % present mask   
+else
+    Screen('DrawDots', G.pWindow, [0, 0], G.fixDotSize*G.pixPerDeg, 0, G.screenCenter, 1);
+    Screen('DrawDots', G.pWindow, [0, 0], 0.5*G.fixDotSize*G.pixPerDeg, G.fixDotColor, G.screenCenter, 1);
+end
+
+% Flip for offset
+Screen('DrawDots', G.pWindow, [0, 0], G.fixDotSize*G.pixPerDeg, 0, G.screenCenter, 1);
+Screen('DrawDots', G.pWindow, [0, 0], 0.5*G.fixDotSize*G.pixPerDeg, G.fixDotColor, G.screenCenter, 1);
+%}
+
+if isnan(data.resp)
+    cfg = [];
+    cfg.validKeys = G.validKeys;
+    cfg.endTime = time + cfg0.ISI - G.pressLag;  
+    [data.resp, log.respTime] = getResponse(cfg);
+end
+
+%% Post-trial stuff
+log.lastOnset = time;
+
+if ~isnan(data.resp)
+    data.RT = log.respTime - timeOnset;
+end
+
+if (cfg0.oddball == ~isnan(data.resp))
+    data.correct = 1;
+else
+    data.correct = 0;
+end
+
+Screen('Close', pStim);
+
+end
